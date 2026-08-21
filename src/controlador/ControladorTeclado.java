d644b942395b9e3b43f76c5864c77e1dd922aac4
LuchitoG <144847003+LuchitoG@users.noreply.github.com>
2026-08-20 22:21:39 -0300

    agregue la deteccion de las flechas para disparar

--- src/controlador/ControladorTeclado.java
+++ src/controlador/ControladorTeclado.java
@@ -7,7 +7,8 @@ public class ControladorTeclado implements KeyListener {
 
     private boolean arriba, abajo, izquierda, derecha;
 
-    // Getters para consultar el estado de movimiento 
+    private boolean dispararArriba, dispararAbajo, dispararIzq, dispararDere;
+    // Getters para consultar el estado de movimiento
 
     public boolean isArriba() {
         return arriba;
@@ -25,26 +26,74 @@ public class ControladorTeclado implements KeyListener {
         return derecha;
     }
 
+    public boolean isDispararArriba() {
+        return dispararArriba;
+    }
+
+    public boolean isDispararAbajo() {
+        return dispararAbajo;
+    }
+
+    public boolean isDispararIzq() {
+        return dispararIzq;
+    }
+
+    public boolean isDispararDere() {
+        return dispararDere;
+    }
+
     // lo que permite que el controlador pueda recibir eventos de teclado
 
+    // MOVIMIENTO
     @Override
     public void keyPressed(KeyEvent e) {
         int key = e.getKeyCode();
-        if (key == KeyEvent.VK_W) arriba = true;
-        if (key == KeyEvent.VK_S) abajo = true;
-        if (key == KeyEvent.VK_A) izquierda = true;
-        if (key == KeyEvent.VK_D) derecha = true;
+        if (key == KeyEvent.VK_W)
+            arriba = true;
+        if (key == KeyEvent.VK_S)
+            abajo = true;
+        if (key == KeyEvent.VK_A)
+            izquierda = true;
+        if (key == KeyEvent.VK_D)
+            derecha = true;
+
+        // DISPARO
+
+        if (key == KeyEvent.VK_UP)
+            dispararArriba = true;
+        if (key == KeyEvent.VK_DOWN)
+            dispararAbajo = true;
+        if (key == KeyEvent.VK_LEFT)
+            dispararIzq = true;
+        if (key == KeyEvent.VK_RIGHT)
+            dispararDere = true;
     }
 
     @Override
     public void keyReleased(KeyEvent e) {
         int key = e.getKeyCode();
-        if (key == KeyEvent.VK_W) arriba = false;
-        if (key == KeyEvent.VK_S) abajo = false;
-        if (key == KeyEvent.VK_A) izquierda = false;
-        if (key == KeyEvent.VK_D) derecha = false;
+        // RELEASE DEL MOVIMIENTO
+        if (key == KeyEvent.VK_W)
+            arriba = false;
+        if (key == KeyEvent.VK_S)
+            abajo = false;
+        if (key == KeyEvent.VK_A)
+            izquierda = false;
+        if (key == KeyEvent.VK_D)
+            derecha = false;
+
+        // RELEASE DEL DISPARO
+        if (key == KeyEvent.VK_UP)
+            dispararArriba = false;
+        if (key == KeyEvent.VK_DOWN)
+            dispararAbajo = false;
+        if (key == KeyEvent.VK_LEFT)
+            dispararIzq = false;
+        if (key == KeyEvent.VK_RIGHT)
+            dispararDere = false;
     }
 
     @Override
-    public void keyTyped(KeyEvent e) {}
+    public void keyTyped(KeyEvent e) {
+    }
 }
