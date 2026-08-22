caa537f956007cfccb7834908ae204340289d732
LuchitoG <144847003+LuchitoG@users.noreply.github.com>
2026-08-22 14:48:49 -0300

    agregado estaFueraPantalla en clase proyectil

--- src/entidades/Proyectil.java
+++ src/entidades/Proyectil.java
@@ -1,89 +1,120 @@
 package entidades;
 
 public abstract class Proyectil {
-    
-private float posicionX;
-private float posicionY;
-private double velocidadProyectil;
-private Integer danoProyectil;
 
-private double direccionX;
-private double direccionY;
+    private float posicionX;
+    private float posicionY;
+    private double velocidadProyectil;
+    private Integer danoProyectil;
 
-private Integer tamanoWidth;
-private Integer tamanoHeight;
+    private double direccionX;
+    private double direccionY;
 
-public Proyectil(float posicionX, float posicionY, double velocidadProyectil, 
-    Integer danoProyectil, double direccionX, double direccionY,
-    Integer tamanoWidth, Integer tamanoHeight){
-    if(velocidadProyectil <= 0.0) throw new IllegalArgumentException("Velocidad de proyectil invalida");
-    if(danoProyectil == null || danoProyectil <= 0) throw new IllegalArgumentException("Daño de proyectil invalido");
-    if (tamanoHeight == null || tamanoHeight <= 0) throw new IllegalArgumentException("valor de altura del proyectil invalido");
-    if (tamanoWidth == null || tamanoWidth <= 0) throw new IllegalArgumentException("valor de ancho del proyectil invalido");
-        
+    private Integer tamanoWidth;
+    private Integer tamanoHeight;
 
-    this.posicionX = posicionX;
-    this.posicionY = posicionY;
-    this.velocidadProyectil = velocidadProyectil;
-    this.danoProyectil = danoProyectil;
+    public Proyectil(float posicionX, float posicionY, double velocidadProyectil,
+            Integer danoProyectil, double direccionX, double direccionY,
+            Integer tamanoWidth, Integer tamanoHeight) {
+        if (velocidadProyectil <= 0.0)
+            throw new IllegalArgumentException("Velocidad de proyectil invalida");
+        if (danoProyectil == null || danoProyectil <= 0)
+            throw new IllegalArgumentException("Daño de proyectil invalido");
+        if (tamanoHeight == null || tamanoHeight <= 0)
+            throw new IllegalArgumentException("valor de altura del proyectil invalido");
+        if (tamanoWidth == null || tamanoWidth <= 0)
+            throw new IllegalArgumentException("valor de ancho del proyectil invalido");
 
-    this.direccionX = direccionX;
-    this.direccionY = direccionY;
+        this.posicionX = posicionX;
+        this.posicionY = posicionY;
+        this.velocidadProyectil = velocidadProyectil;
+        this.danoProyectil = danoProyectil;
 
-    this.tamanoWidth = tamanoWidth;
-    this.tamanoHeight = tamanoHeight;
-    
-}
+        this.direccionX = direccionX;
+        this.direccionY = direccionY;
 
-//Getters
+        this.tamanoWidth = tamanoWidth;
+        this.tamanoHeight = tamanoHeight;
 
-public float getPosicionX(){return posicionX;}
-public float getPosicionY(){return posicionY;}
+    }
 
-public double getVelocidadProyectil(){return velocidadProyectil; }
-public Integer getDanoProyectil(){return danoProyectil; }
+    // Getters
 
-public Integer getTamanoWidth(){return tamanoWidth; }
-public Integer getTamanoHeight(){return tamanoHeight; }
+    public float getPosicionX() {
+        return posicionX;
+    }
 
-//Setters no van, tengo entendido que bugean las "fisicas" de la bala, despues preguntar bien a los profes
+    public float getPosicionY() {
+        return posicionY;
+    }
 
-//Actualizacion de posicion por cada frame
+    public double getVelocidadProyectil() {
+        return velocidadProyectil;
+    }
 
-public void actualizarPosicion(float nuevoX, float nuevoY){
-this.posicionX = nuevoX;
-this.posicionY = nuevoY;
-}
-// No es complejo de pensar, pero tuve que usar IA pq no supe como implementarlo (era una re pavada)
+    public Integer getDanoProyectil() {
+        return danoProyectil;
+    }
 
-/*  Los proyectiles van todo recto
-para eso usaremos una formula matematica algebraica
-arostotelica analitica geometrica angular fisica
-no binarie menta granizada
-*/
-public void mover(){
-    //Formula: Posicion Actual + (Direccion # Velocidad)
-    //Aprendan Albert Epstein y Estifen fukin (dea)
-    float nuevoX = (float) (this.posicionX + (this.direccionX * this.velocidadProyectil));
-    float nuevoY = (float) (this.posicionY + (this.direccionY * this.velocidadProyectil));
+    public Integer getTamanoWidth() {
+        return tamanoWidth;
+    }
 
-    this.actualizarPosicion(nuevoX, nuevoY);
-}
+    public Integer getTamanoHeight() {
+        return tamanoHeight;
+    }
 
-/*Ahora se viene la matematica de las colisiones, 
-voy a usar AABB, busquenlo como matematica de la colision (AABB)
-el mismo tiene 4 premisas, aca las implemento, un saludo al indio alibaba ingeniero de chutub*/
+    // Setters no van, tengo entendido que bugean las "fisicas" de la bala, despues
+    // preguntar bien a los profes
 
-public boolean colisionProyectil(Personaje objetivo){
-    if (objetivo == null) return false;
+    // Actualizacion de posicion por cada frame
 
-    boolean colisionX = this.posicionX < (objetivo.getPosicionX() + objetivo.getTamanoWidth()) &&
-    (this.posicionX + this.tamanoWidth) > objetivo.getPosicionX();
+    public void actualizarPosicion(float nuevoX, float nuevoY) {
+        this.posicionX = nuevoX;
+        this.posicionY = nuevoY;
+    }
+    // No es complejo de pensar, pero tuve que usar IA pq no supe como implementarlo
+    // (era una re pavada)
 
-    boolean colisionY = this.posicionY < (objetivo.getPosicionY() + objetivo.getTamanoHeight()) &&
-    (this.posicionY + this.tamanoHeight) > objetivo.getPosicionY();
-    
-    return colisionX && colisionY;
-}
+    /*
+     * Los proyectiles van todo recto
+     * para eso usaremos una formula matematica algebraica
+     * arostotelica analitica geometrica angular fisica
+     * no binarie menta granizada
+     */
+    public void mover() {
+        // Formula: Posicion Actual + (Direccion # Velocidad)
+        // Aprendan Albert Epstein y Estifen fukin (dea)
+        float nuevoX = (float) (this.posicionX + (this.direccionX * this.velocidadProyectil));
+        float nuevoY = (float) (this.posicionY + (this.direccionY * this.velocidadProyectil));
+
+        this.actualizarPosicion(nuevoX, nuevoY);
+    }
+
+    /*
+     * Ahora se viene la matematica de las colisiones,
+     * voy a usar AABB, busquenlo como matematica de la colision (AABB)
+     * el mismo tiene 4 premisas, aca las implemento, un saludo al indio alibaba
+     * ingeniero de chutub
+     */
+
+    public boolean colisionProyectil(Personaje objetivo) {
+        if (objetivo == null)
+            return false;
+
+        boolean colisionX = this.posicionX < (objetivo.getPosicionX() + objetivo.getTamanoWidth()) &&
+                (this.posicionX + this.tamanoWidth) > objetivo.getPosicionX();
+
+        boolean colisionY = this.posicionY < (objetivo.getPosicionY() + objetivo.getTamanoHeight()) &&
+                (this.posicionY + this.tamanoHeight) > objetivo.getPosicionY();
+
+        return colisionX && colisionY;
+    }
+
+    // Verifica si la bala salio completamente de los bordes de la pantalla
+    public boolean estaFueraDePantalla(int anchoPantalla, int altoPantalla) {
+        return this.posicionX < 0 || this.posicionX > anchoPantalla ||
+                this.posicionY < 0 || this.posicionY > altoPantalla;
+    }
 
 }
