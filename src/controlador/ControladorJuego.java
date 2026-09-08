298f92fc276cc082e51ffd034163df556b25a151
LuchitoG <144847003+LuchitoG@users.noreply.github.com>
2026-09-07 22:31:26 -0300

    Cambio vida maxima inicial heroe

--- src/controlador/ControladorJuego.java
+++ src/controlador/ControladorJuego.java
@@ -13,7 +13,7 @@ public class ControladorJuego {
     public ControladorJuego() {
         // this.jugador = new Heroe("Carlos", 5.0, 5, 5, 10.0f, 10.0f, 10, 10, "jose",
         // // 250, 5); ejemplo de creación de un personaje
-        this.jugador = new Heroe("Carlos", 5, 10, 10, 1, 0.0f, 0.0f, 40, 40, 10, 6);
+        this.jugador = new Heroe("Carlos", 5, 10, 10, 1, 0.0f, 0.0f, 40, 40, 125, 6);
         this.teclado = new ControladorTeclado();
 
         this.vistaJuego = new VistaJuego(jugador, teclado);
