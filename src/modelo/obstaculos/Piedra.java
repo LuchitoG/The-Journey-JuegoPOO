2ebdd3b7e6f4c20f76ede31428ccf64cc3004572
LuchitoG <144847003+LuchitoG@users.noreply.github.com>
2026-09-13 23:04:22 -0300

    Clase Piedra, a medias

---
+++ src/modelo/obstaculos/Piedra.java
@@ -0,0 +1,37 @@
+package modelo.obstaculos;
+
+import java.awt.image.BufferedImage;
+
+public class Piedra extends Obstaculo {
+    private int durabilidad;
+
+    public Piedra(float posicionX, float posicionY, float posicionZ, int ancho, int alto,
+            boolean existe, BufferedImage spriteActual, int durabilidad) {
+        super(true, true, true, posicionX, posicionY, posicionZ, ancho, alto, existe, spriteActual);
+        this.durabilidad = durabilidad;
+    }
+
+    public void recibirDisparo() {
+        if (isDestructible() && durabilidad > 0) {
+            durabilidad--;
+            if (durabilidad <= 0) {
+                romper();
+            }
+        }
+    }
+
+    private void romper() {
+        setBloqueaPersonaje(false);
+        setBloqueaProyectil(false);
+        setExiste(false);
+    }
+
+    private int getDurabilidad() {
+        return durabilidad;
+    }
+
+    public void setDurabilidad(int durabailidad) {
+        this.durabilidad = durabilidad;
+    }
+
+}
